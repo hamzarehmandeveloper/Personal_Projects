08461c81b1d3bf30b96887ee73924247c670f379
Hamza Rehman <hamzarehmandeveloper@gmail.com>
2022-11-01 10:24:05 +0500

    UI design of BMI calculator

--- BMIcalculator/lib/inputpage.dart
+++ BMIcalculator/lib/inputpage.dart
@@ -11,7 +11,61 @@ class inputpage extends StatefulWidget {
 class _inputpageState extends State<inputpage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI Calculator"),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                  color:Color(0xFF1D1E33),
+                  borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color:Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),
+              ),
+            ],
+          )),
+          Expanded(child: Container(
+            margin: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+                color:Color(0xFF1D1E33),
+                borderRadius: BorderRadius.circular(10.0)
+            ),
+          ),),
+
+          Expanded(child: Row(
+            children: <Widget>[
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color:Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),),
+              Expanded(child: Container(
+                margin: EdgeInsets.all(15.0),
+                decoration: BoxDecoration(
+                    color:Color(0xFF1D1E33),
+                    borderRadius: BorderRadius.circular(10.0)
+                ),
+              ),
+              ),
+            ],
+          )),
+
+        ],
+      ),
     );
   }
 }
