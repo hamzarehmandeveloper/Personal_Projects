78fd933ad718e5067a1305c07a6e809d9c1dcb46
Hamza Rehman <hamzarehmandeveloper@gmail.com>
2022-11-01 10:10:02 +0500

    theme data setting for whole app and classes

---
+++ BMIcalculator/lib/inputpage.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+
+class inputpage extends StatefulWidget {
+  const inputpage({Key? key}) : super(key: key);
+
+  @override
+  State<inputpage> createState() => _inputpageState();
+}
+
+class _inputpageState extends State<inputpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
