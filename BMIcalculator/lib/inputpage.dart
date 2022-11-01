3db916da75a3e0edbcb1b0f8aa0a7f256767ebdc
Hamza Rehman <hamzarehmandeveloper@gmail.com>
2022-11-01 10:44:28 +0500

    Code refactor and class of repeating container

--- BMIcalculator/lib/inputpage.dart
+++ BMIcalculator/lib/inputpage.dart
@@ -19,52 +19,47 @@ class _inputpageState extends State<inputpage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                  color:Color(0xFF1D1E33),
-                  borderRadius: BorderRadius.circular(10.0)
-                ),
+              Expanded(child: Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
               ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                    color:Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-              ),
+              Expanded(child: Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
+              ),),
             ],
           )),
-          Expanded(child: Container(
-            margin: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-                color:Color(0xFF1D1E33),
-                borderRadius: BorderRadius.circular(10.0)
-            ),
+          Expanded(child: Repeatcontainercode(
+            colors: Color(0xFF1D1E33),
           ),),
 
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                    color:Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
+              Expanded(child: Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
               ),),
-              Expanded(child: Container(
-                margin: EdgeInsets.all(15.0),
-                decoration: BoxDecoration(
-                    color:Color(0xFF1D1E33),
-                    borderRadius: BorderRadius.circular(10.0)
-                ),
-              ),
-              ),
+              Expanded(child: Repeatcontainercode(
+                colors: Color(0xFF1D1E33),
+              ),),
             ],
           )),
 
         ],
+      ),
+    );
+  }
+}
+
+class Repeatcontainercode extends StatelessWidget {
+
+  Repeatcontainercode ({@required this.colors});
+  Color? colors;
+  @override
+  Widget build(BuildContext context) {
+
+    return Container(
+      margin: EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colors,
+          borderRadius: BorderRadius.circular(10.0)
       ),
     );
   }
