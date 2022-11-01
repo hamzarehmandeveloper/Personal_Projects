4973793a35eee67daf05ddb69a51ddeb9675b2f9
Hamza Rehman <hamzarehmandeveloper@gmail.com>
2022-11-01 11:50:17 +0500

    activation and deactivation of widget by tap

--- BMIcalculator/lib/inputpage.dart
+++ BMIcalculator/lib/inputpage.dart
@@ -3,6 +3,11 @@ import 'container.dart';
 import 'IconText.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
+
+const activecolor = Color(0xFF1D1E33);
+const deactivecolor = Color(0xFF111328);
+
+
 class inputpage extends StatefulWidget {
   const inputpage({Key? key}) : super(key: key);
 
@@ -11,6 +16,19 @@ class inputpage extends StatefulWidget {
 }
 
 class _inputpageState extends State<inputpage> {
+  Color malecolor=deactivecolor;
+  Color femalecolor=deactivecolor;
+
+  void updatecolor(int gender){
+    if(gender==1){
+      malecolor=activecolor;
+      femalecolor=deactivecolor;
+    }
+    if(gender==2){
+      femalecolor=activecolor;
+      malecolor=deactivecolor;
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,18 +39,32 @@ class _inputpageState extends State<inputpage> {
         children: <Widget>[
           Expanded(child: Row(
             children: <Widget>[
-              Expanded(child: Repeatcontainercode(
-                colors: Color(0xFF1D1E33),
-                cardWidget: ReapeatTIwidget(
-                  icondata: FontAwesomeIcons.male,
-                  label: 'Male',
+              Expanded(child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    updatecolor(1);
+                  });
+                },
+                child: Repeatcontainercode(
+                  colors: malecolor,
+                  cardWidget: ReapeatTIwidget(
+                    icondata: FontAwesomeIcons.male,
+                    label: 'Male',
+                  ),
                 ),
               ),),
-              Expanded(child: Repeatcontainercode(
-                colors: Color(0xFF1D1E33),
-                cardWidget: ReapeatTIwidget(
-                  icondata: FontAwesomeIcons.female,
-                  label: 'Female',
+              Expanded(child: GestureDetector(
+                onTap: (){
+                  setState(() {
+                    updatecolor(2);
+                  });
+                },
+                child: Repeatcontainercode(
+                  colors: femalecolor,
+                  cardWidget: ReapeatTIwidget(
+                    icondata: FontAwesomeIcons.female,
+                    label: 'Female',
+                  ),
                 ),
               ),),
             ],
